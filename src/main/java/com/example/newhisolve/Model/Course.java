2fb004e56ba6f3e8a46789679e5015a7d7f1db89
Zho <chlwlgh1011@naver.com>
2024-07-20 15:33:51 +0900

    db: 데이터 베이스 스키마 수정

--- src/main/java/com/example/newhisolve/Model/Course.java
+++ src/main/java/com/example/newhisolve/Model/Course.java
@@ -1,9 +1,14 @@
 package com.example.newhisolve.Model;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "courses")
 public class Course {
 
@@ -13,6 +18,7 @@ public class Course {
     private String name;
     private String code;
     private String language;
+    private String description;
 
     @ManyToOne
     @JoinColumn(name = "professor_id")
@@ -25,54 +31,4 @@ public class Course {
             inverseJoinColumns = @JoinColumn(name = "student_id")
     )
     private List<User> students = new ArrayList<>();
-
-    // Getters and Setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public User getProfessor() {
-        return professor;
-    }
-
-    public void setProfessor(User professor) {
-        this.professor = professor;
-    }
-
-    public List<User> getStudents() {
-        return students;
-    }
-
-    public void setStudents(List<User> students) {
-        this.students = students;
-    }
-
-    public String getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
 }
--- src/main/java/com/example/newhisolve/Model/User.java
+++ src/main/java/com/example/newhisolve/Model/User.java
@@ -2,10 +2,14 @@ package com.example.newhisolve.Model;
 
 import jakarta.persistence.*;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "user")
 public class User {
 
@@ -58,87 +62,4 @@ public class User {
         this.setDepartment(user.getDepartment());
         this.setUniqueId(user.getUniqueId());
     }
-
-    // Getters and Setters
-
-    public String getUniqueId() {
-        return uniqueId;
-    }
-
-    public void setUniqueId(String uniqueId) {
-        this.uniqueId = uniqueId;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getHisnetToken() {
-        return hisnetToken;
-    }
-
-    public void setHisnetToken(String hisnetToken) {
-        this.hisnetToken = hisnetToken;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public List<Course> getCourses() {
-        return courses;
-    }
-
-    public void setCourses(List<Course> courses) {
-        this.courses = courses;
-    }
-
-    public List<Course> getEnrolledCourses() {
-        return enrolledCourses;
-    }
-
-    public void setEnrolledCourses(List<Course> enrolledCourses) {
-        this.enrolledCourses = enrolledCourses;
-    }
-
 }
