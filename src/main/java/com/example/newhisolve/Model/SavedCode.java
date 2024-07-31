735eca5d3a793fdea57492c35405be4b3f1fc0dd
gaeunnnn <wkdrkdms0101@gmail.com>
2024-07-31 11:28:15 +0900

    feat : saved_code 테이블 생성 및 submission 테이블 수정

---
+++ src/main/java/com/example/newhisolve/Model/SavedCode.java
@@ -0,0 +1,47 @@
+package com.example.newhisolve.Model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "saved_code")
+public class SavedCode {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "assignment_id")
+    private Assignment assignment;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private User student;
+
+    @Lob
+    @Column(columnDefinition = "LONGTEXT")
+    private String code;
+
+    @Column(name = "last_saved_date")
+    private LocalDateTime lastSavedDate;
+
+    private String language;
+
+    @Override
+    public String toString() {
+        return "SavedCode{" +
+                "id=" + id +
+                ", assignment=" + (assignment != null ? assignment.getId() : "null") +
+                ", student=" + (student != null ? student.getUsername() : "null") +
+                ", code='" + code + '\'' +
+                ", lastSavedDate=" + lastSavedDate +
+                ", language='" + language + '\'' +
+                '}';
+    }
+}
