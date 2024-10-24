249167caa7abb9ebc70b0e3e3890519b86f59553
Zho <chlwlgh1011@naver.com>
2024-10-24 19:07:23 +0900

    testcommit

--- src/main/java/com/example/newhisolve/NewHisolveApplication.java
+++ src/main/java/com/example/newhisolve/NewHisolveApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NewHisolveApplication {
 public static void main(String[] args) {
         SpringApplication.run(NewHisolveApplication.class, args);
+        //testcommit
     }
 
 }
